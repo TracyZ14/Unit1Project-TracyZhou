039e030cd4dba87591b799420e8c392e7f950481
BT_4N2_07 <TracyZ14@nycstudents.net>
2023-09-21 13:09:12 -0400

    Rounded the variables displayed into monetary form and added comments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        // Asks user for the bill, tip percentage, and number of people
         Scanner s = new Scanner(System.in);
         System.out.print("Bill: $");
         String a = s.nextLine();
@@ -11,14 +12,16 @@ public class Main {
         System.out.print("Number of people: ");
         String c = s.nextLine();
         double numberOfPeople = Double.parseDouble(c);
+        // Calculates the total tip amount, total bill including tip, tip per person, and total per person (bill and tip together)
         double totalTipAmount = bill * (1 + ((tipPercentage / 100)));
         double totalBill = bill + totalTipAmount;
         double tipPerPerson = totalTipAmount / numberOfPeople;
         double totalPerPerson = totalBill / numberOfPeople;
-        System.out.println("Total tip amount: $" + totalTipAmount);
-        System.out.println("Total bill: $" + totalBill);
-        System.out.println("Tip per person: $" + tipPerPerson);
-        System.out.println("Total per person: $" + totalPerPerson);
+        // Displays the results of the calculations above in monetary form
+        System.out.println("Total tip amount: $" + (Math.ceil(totalTipAmount * 100) / 100));
+        System.out.println("Total bill: $" + (Math.ceil(totalBill * 100) / 100));
+        System.out.println("Tip per person: $" + (Math.ceil(tipPerPerson * 100) / 100));
+        System.out.println("Total per person: $" + (Math.ceil(totalPerPerson * 100) / 100));
         s.close();
     }
 }
